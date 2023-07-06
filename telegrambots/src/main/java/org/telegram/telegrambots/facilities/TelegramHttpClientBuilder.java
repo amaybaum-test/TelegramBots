b524fe28f895f4d317299bf3bf108331335a6d58
Alex Maybaum <alex.maybaum@whitesourcesoftware.com>
2023-07-06 21:20:54 +0300

    optimize imports

--- telegrambots/src/main/java/org/telegram/telegrambots/facilities/TelegramHttpClientBuilder.java
+++ telegrambots/src/main/java/org/telegram/telegrambots/facilities/TelegramHttpClientBuilder.java
@@ -1,21 +1,10 @@
 package org.telegram.telegrambots.facilities;
 
 import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.CloseableHttpClient;
-// import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContexts;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.facilities.proxysocketfactorys.HttpConnectionSocketFactory;
-import org.telegram.telegrambots.facilities.proxysocketfactorys.HttpSSLConnectionSocketFactory;
-import org.telegram.telegrambots.facilities.proxysocketfactorys.SocksSSLConnectionSocketFactory;
-import org.telegram.telegrambots.facilities.proxysocketfactorys.SocksConnectionSocketFactory;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by bvn13 on 17.04.2018.
